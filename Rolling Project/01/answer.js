202dc06ce5e746215c3ae70f08bd13e09172c95d
OfekArbeli <ofek.arbeli@Verint.com>
2019-01-26 15:18:22 +0200

    re-order users functions into group proto

--- Rolling Project/01/answer.js
+++ Rolling Project/01/answer.js
@@ -1,60 +1,15 @@
-//-----------------------------USERS---------------------------------------//
-var allUsers = [];
-
-function createUser(userName,password,age){
-    if(allUsers.every(item => {return item.userName !== userName}))
-    {
-        allUsers.push(new User (userName,password,age));
-        return "User created"; 
-    }
-    else
-    {
-        return "User already exist";
-    }
-}
-
-function deleteUser(userName){
-    indexOfUser = allUsers.findIndex(item => {return item.userName === userName});
-    if(indexOfUser>-1)
-    {
-        allUsers.splice(indexOfUser,1);
-        return "User removed"; 
-    }
-    else
-    {
-        return "User doesn't exist";
-    }
-}
-
-function getUsers(){
-    var result =[];
-    allUsers.forEach(item => {result.push(item.userName)});
-    return result;
-}
-
-//User constratur
-function User(userName,password,age){
-    this.userName = userName;
-    this.password = password;
-    this.age = age;
-}
-
-User.prototype.updatePassword = function(newPassword){this.password=newPassword};
-User.prototype.setAge = function(newAge){this.age=newAge};
-
-//------------------------------GROUPS-------------------------------------//
-var allGroups = [];
-
-//group constratur
-function Group(groupName)
-{
+//-------------------------Group constructor:--------------------------------//
+function Group(groupName){
     this.groupName = groupName;
+    this.users = [];
+    this.groups = [];
 }
 
-function createGroup(groupName){
-    if(allGroups.every(item => {return item.groupName !== groupName}))
+//-------------------------Group prototype:--------------------------------//
+Group.prototype.createGroup = function(groupName){
+    if(this.groups.every(item => {return item.groupName !== groupName}))
     {
-        allGroups.push(new Group (groupName));
+        this.groups.push(new Group (groupName));
         return "Group created"; 
     }
     else
@@ -62,12 +17,11 @@ function createGroup(groupName){
         return "Group already exist";
     }
 }
-
-function deleteGroup(groupName){
-    indexOfGroup = allGroups.findIndex(item => {return item.groupName === groupName});
+Group.prototype.deleteGroup = function(groupName){
+    indexOfGroup = this.groups.findIndex(item => {return item.groupName === groupName});
     if(indexOfGroup>-1)
     {
-        allGroups.splice(indexOfGroup,1);
+        this.groups.splice(indexOfGroup,1);
         return "Group removed"; 
     }
     else
@@ -75,52 +29,62 @@ function deleteGroup(groupName){
         return "Group doesn't exist";
     }
 }
-
-function getGroups(){
+Group.prototype.getGroupsFromGroup = function(){
     var result =[];
-    allGroups.forEach(item => {result.push(item.groupName)});
+    this.groups.forEach(item => {result.push(item.groupName)});
+    return result;
+}
+Group.prototype.createUser = function(userName,password,age){
+    if(this.users.every(item => {return item.userName !== userName}))
+    {
+        this.users.push(new User (userName,password,age));
+        return "User created"; 
+    }
+    else
+    {
+        return "User already exist";
+    }
+}
+Group.prototype.deleteUser = function(userName){
+    indexOfUser = this.users.findIndex(item => {return item.userName === userName});
+    if(indexOfUser>-1)
+    {
+        this.users.splice(indexOfUser,1);
+        return "User removed"; 
+    }
+    else
+    {
+        return "User doesn't exist";
+    }
+}
+Group.prototype.getUsersFromGroup = function(){
+    var result =[];
+    this.users.forEach(item => {result.push(item.userName)});
     return result;
 }
 
+//-------------------------User constructor:--------------------------------//
+function User(userName,password,age){
+    this.userName = userName;
+    this.password = password;
+    this.age = age;
+}
+//-------------------------User prototypes:--------------------------------//
+User.prototype.updatePassword = function(newPassword){this.password=newPassword};
+User.prototype.setAge = function(newAge){this.age=newAge};
 
-// function Groups(groupName)
-// {
-//     this.groupName = groupName;
-//     this.users = [];
-//     this.addUser = function(userProfile){
-//         for(var i=0;i<users.length;i++)
-//         {
-//             if(userProfile.userName==this.users[i].userName)
-//                 return "User is already in this group";
-//         }
-//         this.users.push(userProfile);
-//         return "User has been added to the group";
-//     };
-//     this.removeUser = function(){
-//         for(var i=0;i<users.length;i++)
-//         {
-//             if(userProfile.userName==this.users[i])
-//             {
-//                 this.users.splice(i,1);
-//                 return "User has been removed from the group"
-//             }
-//         }
-//         return "User wasn't in the group";
-//     };
-//     this.getUserAndGroup = function(){
-//         var usersAndGroups = [groupName,users];
-//         return usersAndGroups;
-//     };
-// }
+//------------------------Global functions:--------------------------------//
 
-var x = createUser("ofek","2222",28);
-var n = createUser("ofek","2222",28);
-var b = createUser("Moshe","2222",28);
-var z = deleteUser("ofek");
-var y = createUser("ofek","2222",28);
-console.log(x);
-console.log(n);
-console.log(z);
-console.log(y);
-console.log(getUsers());
 
+
+
+//----------------------------Testings-------------------------------------//
+var mainGroup = new Group("MainGroup");
+var subGroup = mainGroup.createGroup("subGroup1");
+var subGroup2 = mainGroup.groups[0].createGroup("subGroup1");
+mainGroup.groups[0].groups[0].createUser("Ofek",1234,23);
+console.log(mainGroup);
+console.log(subGroup);
+console.log(subGroup2);
+console.log(mainGroup);
+console.log(mainGroup.groups[0].groups[0].getUsersFromGroup());
